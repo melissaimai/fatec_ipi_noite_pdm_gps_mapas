55736972b900ce66ab3d75facbc7b1ae84b2c6f8
melissaimai <melissayuri_903@hotmail.com>
2020-03-06 19:29:30 -0300

    aa

---
+++ app/src/main/java/com/example/fatec_ipi_noite_pdm_gps_mapas/MainActivity.java
@@ -0,0 +1,161 @@
+package com.example.fatec_ipi_noite_pdm_gps_mapas;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private static final int GPS_REQUEST_CODE = 1001; //
+
+    private TextView locationTextView;
+
+    private double latitude;
+    private double longitude;
+
+    private void configurarGPS(){
+        locationManager =
+                (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                String s =
+                        String.format(
+                                Locale.getDefault(),
+                                "Lat: %f, Lon: %f",
+                                latitude,
+                                longitude);
+                locationManager
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        locationTextView = findViewById(R.id.locationTextView);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        configurarGPS();
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse(
+                        String.format(
+                                Locale.getDefault(),
+                                "geo:%f.%f?q=restaurantes",
+                                latitude,
+                                longitude
+                        )
+                );
+                Intent intent = new Intent (
+                        Intent.ACTION_VIEW,
+                        uri//identificador de recurso (geolocalizacao nesse caso) objeto identificador
+                );
+                intent.setPackage("com.google.android.apps.maps");
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //já tem a permissao?
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(
+                    locationManager.GPS_PROVIDER,
+                    1000, //tempo minimo em milisegundos para atualizar a app
+                    10, //distancia em metro
+                    locationListener
+            );
+        } else {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    GPS_REQUEST_CODE
+            );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == GPS_REQUEST_CODE){
+            if(grantResults.length > 0 &&
+                    grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(
+                            locationManager.GPS_PROVIDER,
+                            1000,
+                            10,
+                            locationListener
+                    );
+                }
+                else{
+                    Toast.makeText(this, getString(R.string.no_gps_no_app),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+    }
+}
